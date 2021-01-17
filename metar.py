adf1d1aee557968ab475d01a594b1858b21a6590
Geoff Myers <myers.geoffrey@gmail.com>
2021-01-17 00:36:21 -0500

    format temps

--- metar.py
+++ metar.py
@@ -3,20 +3,22 @@ import xmltodict
 from const import BASE_URL
 
 class Wind:
+    ''' stores wind speed and direction '''
     def __init__(self, direction, speed):
         self.dir = int(direction)
         self.speed = int(speed)
 
     def __repr__(self):
         if self.speed > 0:
-            return f'{self.dir:03} @ {self.speed:02} kts'
-        return 'calm        '
+            return f'{self.dir:03} @ {self.speed:02}'
+        return 'calm    '
 
     def raw(self):
         return f'{self.dir:03}{self.speed:02}KT'
 
 
 class CloudLayer:
+    '''an individual cloud layer. stores type of layer and altitude AGL'''
     def __init__(self, sky_condition):
         self.cover = sky_condition['@sky_cover']
         if self.cover == 'CLR':
@@ -39,8 +41,8 @@ class CloudLayer:
         return self.is_overcast() or self.is_broken()
 
 
-
 class Sky:
+    '''stores a list of CloudLayers'''
     def __init__(self, sky_condition):
         self.layers = []
         if isinstance(sky_condition, dict):
@@ -62,31 +64,37 @@ class Sky:
         return str(self.ceiling())
 
 
-
 class Metar:
     def __init__(self, metar_xml_dict):
         self.station = metar_xml_dict['station_id']
         self.obs_time = metar_xml_dict['observation_time']
         self.raw = metar_xml_dict['raw_text']
-        self.temp = metar_xml_dict['temp_c']
-        self.dewpt = metar_xml_dict['dewpoint_c']
+        self.temp = round(float(metar_xml_dict['temp_c']))
+        self.dewpt = round(float(metar_xml_dict['dewpoint_c']))
         self.wind = Wind(metar_xml_dict['wind_dir_degrees'],
                          metar_xml_dict['wind_speed_kt'])
-        self.vis = metar_xml_dict['visibility_statute_mi']
+        self.vis = int(float(metar_xml_dict['visibility_statute_mi']))
         self.alt = int(float(metar_xml_dict['altim_in_hg'])*100)/100
-        self.cat =  metar_xml_dict['flight_category']
+        self.cat = metar_xml_dict['flight_category']
         self.sky = Sky(metar_xml_dict['sky_condition'])
+
+    def raw_temp(self):
+        temp_sign = ""
+        dewp_sign = ""
+        if self.temp < 0: temp_sign = "M"
+        if self.dewpt < 0: dewp_sign = "M"
+        temp = f'{temp_sign}{abs(self.temp):02}'
+        dewpt = f'{dewp_sign}{abs(self.dewpt):02}'
+        return  temp + '/' + dewpt
 
     def __repr__(self):
         return f'{self.station} '\
                f'{self.cat:4}  '\
                f'{self.wind}  '\
-               f'{self.vis:4}  '\
                f'{self.alt:4}  '\
+               f'{self.raw_temp():7}  '\
+               f'{self.vis:02}  '\
                f'{self.sky}'
-
-
-
 
 
 airports = "KTTA KSEA KRDU KHQM KGSO KPIT KPIA"
