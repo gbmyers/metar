7b2e8758391ec6f497e8b4fd1b0dadcada013869
Geoff Myers <myers.geoffrey@gmail.com>
2021-01-17 19:24:51 -0500

    more pythonic

--- const.py
+++ const.py
@@ -1,3 +1,9 @@
 
 
-BASE_URL = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=1&mostRecentForEachStation=true&stationString="
+BASE_URL = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?"\
+           "dataSource=metars&"\
+           "requestType=retrieve&"\
+           "format=xml&"\
+           "hoursBeforeNow=1&"\
+           "mostRecentForEachStation=true&"\
+           "stationString="
