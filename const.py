ac079375034cab82771c8e9c52e6f3ebb9eb98b0
Geoff Myers <myers.geoffrey@gmail.com>
2021-01-22 21:48:06 -0500

    Delete const.py
    moved constants into main module

--- const.py
+++
@@ -1,9 +0,0 @@
-
-
-BASE_URL = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?"\
-           "dataSource=metars&"\
-           "requestType=retrieve&"\
-           "format=xml&"\
-           "hoursBeforeNow=1&"\
-           "mostRecentForEachStation=true&"\
-           "stationString="
